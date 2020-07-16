4b52f38ce787cb905b44bbf59f67fa44db5257b9
Gnlow <entrydark@gmail.com>
2020-07-16 11:18:26 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 const decoder = new TextDecoder("utf-8")
 
 const f = await Deno.open("sample.txt")
-console.log(decoder.decode(await Deno.readAll(f))) // Hello, World!
+export default decoder.decode(await Deno.readAll(f)) // Hello, World!
 f.close()
